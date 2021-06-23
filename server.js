20e58d0f9b4eb4928b17897dc209c25f4bc707f9
Jorge Viera <Jorge5psgt@gmail.com>
2021-06-23 14:49:05 -0400

    finished

--- server.js
+++ server.js
@@ -83,15 +83,15 @@ app.delete("/api/notes/:id", async(req, res) => {
     //      res.send("saved note");
     //})
 
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "/public/index.html"));
-    });
+    // app.get("/", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "/public/index.html"));
+    // });
     app.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, "/public/notes.html"))
     });
     
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'))
+        res.sendFile(path.join(__dirname, '/public/index.html'))
     });
     
     
